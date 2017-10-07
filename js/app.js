494420d1ba202b5906c47473d30cb3440edebaba
Vu Nguyen <vu.nguyen@autogravity.com>
2017-10-07 12:21:56 -0700

    this is my first commit for this project.

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(".navigation-bar").sticky();
+
+$(document).ready(function() {
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+});
+
+const menu = document.querySelector('.menu-icon');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-icon');
+const linkArrow = document.querySelector('.link-arrow');
+const arrowIcon = document.querySelector('.icon-img');
+
+menu.addEventListener('click', () => {
+  overlay.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
+linkArrow.addEventListener('click', () => {
+  arrowIcon.style.transform = 'translate(20px)';
+});
